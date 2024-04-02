f40a25b10471a41dc9d3ba35607cad18fc889f07
zeroprin-zf <zerozerozf07@outlook.jp>
2024-04-02 19:21:33 +0900

    練習問題1

---
+++ src/array_exercrises/java/exercrises.java
@@ -0,0 +1,14 @@
+package array_exercrises.java;
+
+public class exercrises {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
+	}
+
+}
