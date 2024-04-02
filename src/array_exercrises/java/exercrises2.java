d4f3764c04c473cb9eba669e4ccce3152d4cc74b
zeroprin-zf <zerozerozf07@outlook.jp>
2024-04-02 19:34:08 +0900

    練習問題2

---
+++ src/array_exercrises/java/exercrises2.java
@@ -0,0 +1,18 @@
+package array_exercrises.java;
+
+public class exercrises2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int[] moneyList;
+		moneyList = new int[] {121902, 8302, 55100};
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+		for (int m : moneyList) {
+			System.out.println(m);
+		}
+	}
+
+}
